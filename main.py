d654458c8449ee05b39681a5d81d3dc0063eed6e
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-10-20 10:21:39 +0200

    black format

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-import matplotlib.pyplot as plt 
-import pandas as pd 
+import matplotlib.pyplot as plt
+import pandas as pd
 
 import utilities
 
-import warnings 
-warnings.filterwarnings('ignore') 
+import warnings
 
-class Application :
-	def __init__(self) -> None:
-		self.data_processor = utilities.ProcessData()
-		self.visualize = utilities.Visualization()
-		self.model_handler = utilities.HandleModel()
-	def main(self):
-		# path to the folder containing our dataset 
-		dataset = 'data/traffic_Data/DATA'
+warnings.filterwarnings("ignore")
 
-		# path of label file 
-		label_file = pd.read_csv('data/labels.csv') 
 
-		train_ds,val_ds = self.data_processor.load_datasets(dataset)
-		# self.visualize.show_sample_data(train_ds, label_file)
-		input_shape = (224, 224, 3)
-		model = self.model_handler.create_model(input_shape,label_file)
-		
+class Application:
+    def __init__(self) -> None:
+        self.data_processor = utilities.ProcessData()
+        self.visualize = utilities.Visualization()
+        self.model_handler = utilities.HandleModel()
+        self.DATASET = "data/traffic_Data/DATA"
 
-		print(model.summary())
+    def main(self):
+        # path to the folder containing our dataset
 
-		model = self.model_handler.compile_model(model)
-		
-		# Set callback functions to early stop training 
-		history = self.model_handler.train_model(model,train_ds,val_ds)
+        # path of label file
+        label_file = pd.read_csv("data/labels.csv")
 
-		self.visualize.show_loss_accuracy(history)
+        train_ds, val_ds = self.data_processor.load_datasets(self.DATASET)
+        # self.visualize.show_sample_data(train_ds, label_file)
+        input_shape = (224, 224, 3)
+        model = self.model_handler.create_model(input_shape, label_file)
+
+        print(model.summary())
+
+        model = self.model_handler.compile_model(model)
+
+        # Set callback functions to early stop training
+        history = self.model_handler.train_model(model, train_ds, val_ds)
+
+        self.visualize.show_loss_accuracy(history)
+
 
 if __name__ == "__main__":
     app = Application()
--- utilities.py
+++ utilities.py
@@ -1,105 +1,116 @@
-import tensorflow as tf 
-import matplotlib.pyplot as plt 
-from tensorflow.keras.layers import Conv2D, MaxPooling2D 
-from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense 
-from tensorflow.keras.models import Sequential 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.callbacks import EarlyStopping 
+from tensorflow.keras.callbacks import EarlyStopping
+
 
 class ProcessData:
-    def load_datasets(self,dataset):
-        train_ds = tf.keras.preprocessing.image_dataset_from_directory(dataset, validation_split=0.2, 
-                                                            subset='training', 
-                                                            image_size=(224, 224), 
-                                                            seed=123, 
-                                                            batch_size=32) 
-        val_ds = tf.keras.preprocessing.image_dataset_from_directory(dataset, validation_split=0.2, 
-                                                                    subset='validation', 
-                                                                    image_size=(224, 224), 
-                                                                    seed=123, 
-                                                                    batch_size=32)
+    def load_datasets(self, dataset):
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            dataset,
+            validation_split=0.2,
+            subset="training",
+            image_size=(224, 224),
+            seed=123,
+            batch_size=32,
+        )
+        val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            dataset,
+            validation_split=0.2,
+            subset="validation",
+            image_size=(224, 224),
+            seed=123,
+            batch_size=32,
+        )
 
         return train_ds, val_ds
-    
+
+
 class Visualization:
     def __init__(self) -> None:
         pass
 
-    def show_sample_data(self, train_ds,label_file):
-        class_numbers = train_ds.class_names 
-        class_names = [] 
-        for i in class_numbers: 
-            class_names.append(label_file['Name'][int(i)]) 
+    def show_sample_data(self, train_ds, label_file):
+        class_numbers = train_ds.class_names
+        class_names = []
+        for i in class_numbers:
+            class_names.append(label_file["Name"][int(i)])
 
-            
-        plt.figure(figsize=(10, 10)) 
-        for images, labels in train_ds.take(1): 
-            for i in range(25): 
-                ax = plt.subplot(5, 5, i + 1) 
-                plt.imshow(images[i].numpy().astype("uint8")) 
-                plt.title(class_names[labels[i]]) 
-                plt.axis("off") 
-        plt.show() 
-
-    def show_loss_accuracy(self,history):
-        # Loss 
-        plt.plot(history.history['loss']) 
-        plt.plot(history.history['val_loss']) 
-        plt.legend(['loss', 'val_loss'], loc='upper right') 
-
-        # Accuracy 
-        plt.plot(history.history['accuracy']) 
-        plt.plot(history.history['val_accuracy']) 
-        plt.legend(['accuracy', 'val_accuracy'], loc='upper right') 
+        plt.figure(figsize=(10, 10))
+        for images, labels in train_ds.take(1):
+            for i in range(25):
+                ax = plt.subplot(5, 5, i + 1)
+                plt.imshow(images[i].numpy().astype("uint8"))
+                plt.title(class_names[labels[i]])
+                plt.axis("off")
         plt.show()
+
+    def show_loss_accuracy(self, history):
+        # Loss
+        plt.plot(history.history["loss"])
+        plt.plot(history.history["val_loss"])
+        plt.legend(["loss", "val_loss"], loc="upper right")
+
+        # Accuracy
+        plt.plot(history.history["accuracy"])
+        plt.plot(history.history["val_accuracy"])
+        plt.legend(["accuracy", "val_accuracy"], loc="upper right")
+        plt.show()
+
 
 class HandleModel:
     def __init__(self) -> None:
         pass
 
-    def create_model(self,input_shape,label_file):
-        data_augmentation = tf.keras.Sequential( 
-			[ 
-				tf.keras.layers.experimental.preprocessing.RandomFlip( 
-					"horizontal", input_shape=input_shape), 
-				tf.keras.layers.experimental.preprocessing.RandomRotation(0.1), 
-				tf.keras.layers.experimental.preprocessing.RandomZoom(0.2), 
-				tf.keras.layers.experimental.preprocessing.RandomFlip( 
-					mode="horizontal_and_vertical") 
-			] 
-		) 
+    def create_model(self, input_shape, label_file):
+        data_augmentation = tf.keras.Sequential(
+            [
+                tf.keras.layers.experimental.preprocessing.RandomFlip(
+                    "horizontal", input_shape=input_shape
+                ),
+                tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+                tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
+                tf.keras.layers.experimental.preprocessing.RandomFlip(
+                    mode="horizontal_and_vertical"
+                ),
+            ]
+        )
 
-        model = Sequential() 
-        model.add(data_augmentation) 
-        model.add(Rescaling(1./255)) 
-        model.add(Conv2D(128, (3, 3), activation='relu')) 
-        model.add(MaxPooling2D((2, 2))) 
-        model.add(Conv2D(64, (3, 3), activation='relu')) 
-        model.add(MaxPooling2D((2, 2))) 
-        model.add(Conv2D(128, (3, 3), activation='relu')) 
-        model.add(MaxPooling2D((2, 2))) 
-        model.add(Conv2D(256, (3, 3), activation='relu')) 
-        model.add(MaxPooling2D((2, 2))) 
-        model.add(Flatten()) 
-        model.add(Dense(64, activation='relu')) 
-        model.add(Dropout(0.2)) 
-        model.add(Dense(128, activation='relu')) 
-        model.add(Dense(len(label_file), activation='softmax')) 
+        model = Sequential()
+        model.add(data_augmentation)
+        model.add(Rescaling(1.0 / 255))
+        model.add(Conv2D(128, (3, 3), activation="relu"))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(128, (3, 3), activation="relu"))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(256, (3, 3), activation="relu"))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(64, activation="relu"))
+        model.add(Dropout(0.2))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dense(len(label_file), activation="softmax"))
 
         return model
-    
+
     def compile_model(self, model):
-        model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), 
-                            optimizer='adam', 
-                            metrics=['accuracy'])
-        
+        model.compile(
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            optimizer="adam",
+            metrics=["accuracy"],
+        )
+
         return model
 
-    def train_model(self, model,train_ds,val_ds):
-        mycallbacks = [EarlyStopping(monitor='val_loss', patience=5)] 
-        history = model.fit(train_ds, 
-                        validation_data=val_ds, 
-                        epochs=3, 
-                        callbacks=mycallbacks)
-        
+    def train_model(self, model, train_ds, val_ds):
+        mycallbacks = [EarlyStopping(monitor="val_loss", patience=5)]
+        history = model.fit(
+            train_ds, validation_data=val_ds, epochs=3, callbacks=mycallbacks
+        )
+
         return history
